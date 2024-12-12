73c3fb2d2fb569e7905523319fbc79123c0ea078
Ruslan Voronenko <105813644+RusProgger@users.noreply.github.com>
2024-12-12 11:11:07 +0200

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,4 +1,27 @@
-const menu = document.querySelector('.burger-menu').onclick = function() {
-    document.querySelector('.menu__list').classList.toggle('active-menu')
-    document.querySelector('.burger-menu').classList.toggle('active');
+const burgerMenu = document.querySelector('.burger-menu');
+const menuList = document.querySelector('.menu__list');
+
+// Обработчик клика на иконку бургера
+burgerMenu.onclick = function() {
+    menuList.classList.toggle('active-menu');
+    burgerMenu.classList.toggle('active');
 };
+
+// Закрытие меню при клике на ссылку
+const menuLinks = document.querySelectorAll('.menu__list a'); // Селектор для всех ссылок в меню
+
+menuLinks.forEach(link => {
+    link.onclick = function() {
+        menuList.classList.remove('active-menu');
+        burgerMenu.classList.remove('active');
+    };
+});
+
+// Закрытие меню при клике вне области меню
+document.addEventListener('click', function(event) {
+    // Проверяем, был ли клик вне меню или кнопки бургера
+    if (!burgerMenu.contains(event.target) && !menuList.contains(event.target)) {
+        menuList.classList.remove('active-menu');
+        burgerMenu.classList.remove('active');
+    }
+});
